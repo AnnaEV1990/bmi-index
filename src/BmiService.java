dba05db4a0da63df11c6d7688c4db8dc1cc1eac3
Anna Evseykina <anytta9032@yandex.ru>
2024-02-03 17:58:31 +0700

    change formula

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
-    public int calculate(int weight_kg, int height_m) {
-        return weight_kg / height_m * height_m;
+    public double calculate(int weight_kg, double height_m) {
+        double index = weight_kg / (height_m * height_m);
+        return (int) index;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        System.out.println("63, 1.62");
-        System.out.println(service.calculate( 63, (int) 1.62));
+        System.out.println((int) service.calculate(63, 1.62));
 
     }
 }
